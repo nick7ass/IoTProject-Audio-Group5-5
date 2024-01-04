f3115190a532526a730386a597c69a2628fa0d24
Nicklas Bourelius <145897592+nick7ass@users.noreply.github.com>
2024-01-04 10:31:26 +0100

    Finished product, more or less
    Everything working as expected.
    Unnecessary code removed.
    Cleaned code and comments for added clarity added and modified.

--- app/src/main/java/com/example/group5_iotproject_mnz/MainActivity.java
+++ app/src/main/java/com/example/group5_iotproject_mnz/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.group5_iotproject_mnz;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -29,43 +25,42 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Mqtt client and mosquitto for connection with raspberry pi
+    //Mqtt client and mosquitto for connection with broker
     private MqttAndroidClient client;
-    private static final String SERVER_URI = "tcp://192.168.0.81:1883";
-            //"tcp://test.mosquitto.org:1883";
+
+    //SERVER_URI holds the IP address of the Raspberry Pi as it has the broker on it.
+    //The tcp is first the IPv4 address followed by the port used
+    private static final String SERVER_URI = "tcp://192.168.0.83:1883";
+
+    //TAG variable used for logging/debugging purposes. Not completely necessary for this project in particular but using such tags is a good habit to build
     private static final String TAG = "MainActivity";
 
-    private String voiceCommandFinal;
+    //Variable to hold voiceCommands
+    private String voiceCommandFinal = "";
 
+    //Variables for the speech recognizer:
     private SpeechRecognizer speechRecognizer;
     private Intent intentRecognizer;
-    private TextView textView;
 
-    //Imageview of lightbulb
-    ImageView lightImage;
+    //Simply used for debugging purposes/making sure everything works properly on this end (Hidden in UI if not debugging)
+    //private TextView textView;
+    //ImageView lightImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Ask permission for using recording
+        //Ask permission for using recording on device
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PackageManager.PERMISSION_GRANTED);
 
-        //Getting the textview and imageview from ID
-        textView = findViewById(R.id.infoTextView);
-        lightImage = findViewById(R.id.lightImage);
-        voiceCommandFinal = "dimmed";
-
-        //Buttons Not needed because of made their methods connected through their attribute in xml
-        //voiceCommandButton = findViewById(R.id.voiceCommandButton);
-        //stopButton = findViewById(R.id.stopButton);
+        //Getting the textview and imageview from id (only used for debugging purposes)
+        //textView = findViewById(R.id.infoTextView);
+        //lightImage = findViewById(R.id.lightImage);
 
         //Creating and initiating the necessary parts for speechrecognizer
         intentRecognizer = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -73,8 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         //Initiation of the listener automatically creates the different methods below.
-        //Will primarily/only use onResults and error at least at first
-        //If we remove buttons then maybe we will use other methods as well
+        //Will primarily/only use onResults
         speechRecognizer.setRecognitionListener(new RecognitionListener() {
             @Override
             public void onReadyForSpeech(Bundle bundle) {
@@ -103,64 +97,44 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(int i) {
-                //textView.setText(getString(R.string.error_message_voiceCommand));
+
             }
 
             @Override
             public void onResults(Bundle bundle) {
                 //Makes an arraylist of the input from the recognizer
                 ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                String voiceCommand = "";
-                //If the input is not nothing then it will create a string of the first input (on index 0)
+                //String voiceCommand = "";
+
+                //If the input is not null then it will create a string of the first input (on index 0)
                 //and then set the text of the textview to the string (to see how the input turns out)
                 //This will then be changed to make the lights change depending on the voice commands
                 if(matches != null) {
-                    //Here code for Actuating/Simulating actuation should be
-                    //Ta fram en method som typ kollar om stringen innehåller "dim" så ska
-                    //ljuset bli dimmat men om det innehåller off så ska det off och on så on
-                    //Lägg också till att det här ska anpassas utifrån tid?
-                    voiceCommand = matches.get(0);
-                    //textView.setText(voiceCommand);
+                    String voiceCommand = matches.get(0);
 
-                    //To be able to consider time: (Couldn't use "LocalTime" as we need API min of 22 to work since thats the only android phone available to us
-
-                    //To get current time
-                    /*Calendar calendar  = Calendar.getInstance();
-                    int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-
-                    //Our time range for which to consider when controlling lights
-                    int startHourRange = 6;
-                    int endHourRange = 17;*/
-
-                    //Change the light depending on input:
-
-                    //Dim the lights if the command includes dim
+                    //Change the light depending on input by setting the voiceCommandFinal variable:
+                    //Commented out Image and Textview used for debugging purposes
                     if (voiceCommand.contains("dim")) {
                         voiceCommandFinal = "dimmed";
-                        lightImage.setImageResource(R.drawable.lightdim);
-                        textView.setText("Lights dimmed.");
-                    } //Turn off lights if command contains off
+                        //lightImage.setImageResource(R.drawable.lightdim);
+                        //textView.setText("Lights dimmed.");
+                    }
                     else if (voiceCommand.contains("off")) {
                         voiceCommandFinal = "off";
-                        lightImage.setImageResource(R.drawable.lightoffcompletely);
-                        textView.setText("Lights turned off.");
-                    } //If it's daytime turn lights on bright
-                   /* else if (voiceCommand.contains("on") && hourOfDay >= startHourRange && hourOfDay <= endHourRange) {
-                        voiceCommandFinal = "on";
-                        lightImage.setImageResource(R.drawable.lightonbright);
-                        textView.setText("Lights turned on bright."); } */
-
-                //If it's late then turn them on but warmer
-                    else if (voiceCommand.contains("on")) {
-                        voiceCommandFinal = "on"; //removed "onWarm" and made it "on" because checks time in simulator but either works but redundant in both
-                        lightImage.setImageResource(R.drawable.lightonwarm);
-                        textView.setText("Lights set to 'On' but warmer since it's late.");
+                        //lightImage.setImageResource(R.drawable.lightoffcompletely);
+                        //textView.setText("Lights turned off.");
                     }
-                } //If something goes wrong write out error message (Is this needed?)
-                else {
-                    textView.setText(getString(R.string.error_message_noInput_voiceCommand));
+                    else if (voiceCommand.contains("on")) {
+                        voiceCommandFinal = "on";
+                       //lightImage.setImageResource(R.drawable.lightonwarm);
+                        //textView.setText("Lights set to 'On' but warmer since it's late.");
+                    }
                 }
+                //If something goes wrong write out error message
+                //else {//textView.setText(getString(R.string.error_message_noInput_voiceCommand));}
 
+                //Starts publish method with the voice command as parameter:
+                publish(voiceCommandFinal);
             }
 
             @Override
@@ -174,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Calling method connect below for connection with rasp
+        //Calling method connect below for connection with broker
         connect();
 
         //Callback. What to do when connected
@@ -183,19 +157,8 @@ public class MainActivity extends AppCompatActivity {
             public void connectComplete(boolean reconnect, String serverURI) {
                 if (reconnect) {
                     System.out.println("Reconnected to : " + serverURI);
-                    // Re-subscribe as we lost it due to new session
-                    //subscribe(" YOUR-TOPIC"); //Byt ut yourtopic till the topic, typ iotproject/nmz/lights
-
-                    //Testar publish här:
-                    publish(voiceCommandFinal);
-                    //Slut på test av publish här.
                 } else {
                     System.out.println("Connected to: " + serverURI);
-                    //Testar publish här:
-                    publish(voiceCommandFinal);
-                    //Slut på test av publish här.
-
-                    //subscribe(" YOUR-TOPIC");
                 }
             }
             @Override
@@ -207,11 +170,6 @@ public class MainActivity extends AppCompatActivity {
                     Exception {
                 String newMessage = new String(message.getPayload());
                 System.out.println("Incoming message: " + newMessage);
-
-                 /* add code here to interact with elements
-                 (text views, buttons)
-                 using data from newMessage
-                 */
             }
             @Override
             public void deliveryComplete(IMqttDeliveryToken token) {
@@ -220,8 +178,11 @@ public class MainActivity extends AppCompatActivity {
 //End of callback section
 
     }
+    //OnCreate slut
 
-    //Initializing and creating connection to the broker set in "SERVER_URI", in this case "tcp://test.mosquitto.org:1883"
+
+
+    //Initializing and creating connection to the broker set in "SERVER_URI", in this case the IPv4 of the Raspberry pi with the broker and the port
     private void connect(){
         String clientId = MqttClient.generateClientId();
         client =
@@ -232,17 +193,15 @@ public class MainActivity extends AppCompatActivity {
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
+                    //Connection successful:
                     Log.d(TAG, "onSuccess");
                     System.out.println(TAG + " Success. Connected to " + SERVER_URI);
                 }
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception)
                 {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    Log.d(TAG, "onFailure");
+                    //If something went wrong when trying to connect:                    Log.d(TAG, "onFailure");
                     System.out.println(TAG + " Oh no! Failed to connect to " + SERVER_URI);
-                    exception.printStackTrace();
                 }
             });
         } catch (MqttException e) {
@@ -252,17 +211,14 @@ public class MainActivity extends AppCompatActivity {
     //End of connect method
 
     //Method for publishing to topic
-    //Takes the String lightMode which will be the mode for the light, eg off, on, dimmed, etc.
+    //Takes the String lightMode which will be the mode for the light.
     private void publish(String lightMode) {
-        final String message = lightMode;
-        int qos = 1;
         String topic = "lightModeTopic";
         MqttMessage mqttMessage = new MqttMessage();
-        mqttMessage.setPayload(message.getBytes());
+        mqttMessage.setPayload(lightMode.getBytes());
         try {
-
+            System.out.println("Voice command: " + lightMode + " was sent.");
             client.publish(topic, mqttMessage);
-
         } catch (MqttException e) {
             e.printStackTrace();
         }
@@ -271,9 +227,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     //Method that starts when "Voice Command" button is pressed
-    //It will start listening for the voice commands
+    //It will start listening for the voice command
     public void StartButton(View view){
         speechRecognizer.startListening(intentRecognizer);
     }
+    //End of method that starts when "Voice Command" button is pressed
 
 }
